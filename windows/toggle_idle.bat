f1152f97dfcbbe610e427390117b59c0e873c1ee
Purnama Yasa <yasapurnama@gmail.com>
2021-02-24 21:34:41 +0800

    fix typo

--- windows/toggle_idle.bat
+++ windows/toggle_idle.bat
@@ -8,13 +8,13 @@ goto commonexit
 
 :prevent
 C:\Strawberry\perl\bin\perl.exe -pi -e "s/(; )(.*)(; PreventIdle)/$2$3/g" "%userprofile%\.kinto\kinto.ahk"
-C:\Strawberry\perl\bin\perl.exe -pi -e "s/Prevet Screen Idle, prevent_idle/Prevet Screen Idle, allow_idle/g" "%userprofile%\.kinto\kinto.ahk"
+C:\Strawberry\perl\bin\perl.exe -pi -e "s/Prevent Screen Idle, prevent_idle/Prevent Screen Idle, allow_idle/g" "%userprofile%\.kinto\kinto.ahk"
 "%userprofile%\.kinto\kinto-start.vbs"
 goto commonexit
 
 :allow
 C:\Strawberry\perl\bin\perl.exe -pi -e "s/^(?!;)(.*)(; PreventIdle)/; $1$2/gm" "%userprofile%\.kinto\kinto.ahk"
-C:\Strawberry\perl\bin\perl.exe -pi -e "s/Prevet Screen Idle, allow_idle/Prevet Screen Idle, prevent_idle/g" "%userprofile%\.kinto\kinto.ahk"
+C:\Strawberry\perl\bin\perl.exe -pi -e "s/Prevent Screen Idle, allow_idle/Prevent Screen Idle, prevent_idle/g" "%userprofile%\.kinto\kinto.ahk"
 "%userprofile%\.kinto\kinto-start.vbs"
 goto commonexit
 
